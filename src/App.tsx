a07b83734811004eb2dca659da6fe02a80d8a97b
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2024-04-10 23:04:28 -0300

    feat: finalized

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { AppBar } from './components/AppBar';
+import { TodosList } from './components/TodosList';
+import { useRenderCounter } from './hooks/useRenderCounter';
+import './store/createStore';
+
+export function App() {
+  useRenderCounter('App');
+
+  return (
+    <>
+      <AppBar />
+      <TodosList />
+    </>
+  );
+}
